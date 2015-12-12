c4c3e16fe7becf72b315c62f18fd92f01c7cf745
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-12-12 11:39:57 +0200

    Implement Save() method for Cassette type

--- cassette/cassette.go
+++ cassette/cassette.go
@@ -56,3 +56,35 @@ type Cassette struct {
 	// Interactions between client and server
 	Interactions []Interaction `yaml:"interactions"`
 }
+
+// Saves the cassette on disk for future use
+func (c *Cassette) Save() error {
+	cassetteFile := filepath.Join(c.Name, ".yaml")
+	cassetteDir := filepath.Dir(cassetteFile)
+
+	// Create directory for cassette if missing
+	if _, err := os.Stat(cassetteDir); os.IsNotExist(err) {
+		if err = os.MkdirAll; err != nil {
+			return err
+		}
+	}
+
+	// Marshal to YAML and save interactions
+	data, err := yaml.Marshal(c)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(cassetteFile)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
