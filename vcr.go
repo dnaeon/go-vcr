6498ea0a2f03c52b0f7663f32da3bf7f53fdf580
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-12-14 11:55:48 +0200

    Commit vcr.Play helper function

--- vcr.go
+++ vcr.go
@@ -1,43 +1,21 @@
 package vcr
 
 import (
-	"net/http"
+	"log"
+
+	"github.com/dnaeon/vcr/recorder"
 )
 
-// Client request record type
-type RequestRecord {
-	// Body of request
-	Body string `yaml:"body"`
+// Helper wrapper around recorder.Recorder type
+// The returned Recorder must be stopped by the
+// caller when no longer needed
+func Play(cassette string) *recorder.Recorder {
+	r = recorder.NewRecorder(cassette)
 
-	// Request headers
-	Headers http.Header `yaml:"headers"`
+	err := r.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// Request URL
-	URL string `yaml:"url"`
-
-	// Request method
-	Method string `yaml:"method"`
-}
-
-// Server response record type
-type ResponseRecord struct {
-	// Body of response
-	Body string `yaml:"body"`
-
-	// Response headers
-	Headers http.Header `yaml:"headers"`
-
-	// Response status message
-	Status string `yaml:"status"`
-
-	// Response status code
-	Code int `yaml:"code"`
-}
-
-// CassetteRecord type represents an HTTP interaction between
-// client and server. It contains the client request and the
-// returned response from the server for this request.
-type CassetteRecord struct {
-	Request  RequestRecord  `yaml:"request"`
-	Response ResponseRecord `yaml:"response"`
+	return r
 }
