d0a9be143e966f1d56b003e35857e8a25f22e8cc
Marin Atanasov Nikolov <dnaeon@gmail.com>
2022-08-18 21:02:09 +0300

    Commit test for unmarshaling body into a JSON object

---
+++ examples/json_test.go
@@ -0,0 +1,84 @@
+// Copyright (c) 2015-2022 Marin Atanasov Nikolov <dnaeon@gmail.com>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer
+//    in this position and unchanged.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+// NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+package vcr_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"gopkg.in/dnaeon/go-vcr.v2/recorder"
+)
+
+// payload is a stripped down version of the payload as received from
+// https://httpbin.org/anything
+type payload struct {
+	Data    string            `json:"data"`
+	Headers map[string]string `json:"headers"`
+	Method  string            `json:"method"`
+	Origin  string            `json:"origin"`
+	URL     string            `json:"url"`
+	JSON    any               `json:"json"`
+}
+
+func TestJSON(t *testing.T) {
+	r, err := recorder.New("fixtures/json-content-type")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Stop() // Make sure recorder is stopped once done with it
+
+	if r.Mode() != recorder.ModeRecordOnce {
+		t.Fatal("Recorder should be in ModeRecordOnce")
+	}
+
+	client := r.GetDefaultClient()
+	url := "https://httpbin.org/anything"
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer foo")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	var p payload
+	if err := json.NewDecoder(resp.Body).Decode(&p); err != nil {
+		t.Fatal(err)
+	}
+
+	if p.Method != http.MethodGet {
+		t.Fatalf("expected method was GET, got %q", p.Method)
+	}
+
+	if p.URL != url {
+		t.Fatalf("want url %q, got %q", url, p.URL)
+	}
+
+	if p.Origin == "" {
+		t.Fatalf("expected non-empty origin, got %q", p.Origin)
+	}
+}
